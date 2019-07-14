f822d2fd6dbeae20f9987653dd90b670f479b31d
Aymen <amprodz15@gmail.com>
2019-07-14 02:18:09 -0400

    added jobs db

---
+++ src/data/jobs.js
@@ -0,0 +1,56 @@
+const jobs = [
+    {   
+        id:"job1",
+        type:"engineering",
+        jobTtitle:"User Experience Designer",
+        rating:"4.2/5",
+        location:"Richmond, VA",
+        company:"Google inc.",
+        p1:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid animi delectus deleniti dicta dolor ducimus excepturi facere in, inventore ipsa iste nemo neque nulla odio officia pariatur, perspiciatis quia quis quos rerum, sunt tenetur Ad dolore earum ipsum unde.",
+        p2:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid animi delectus deleniti dicta dolor ducimus excepturi facere in, inventore ipsa iste nemo neque nulla odio officia pariatur, perspiciatis quia quis quos rerum, sunt tenetur!Ad dolore earum ipsum unde.",
+        p3:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid animi delectus deleniti dicta dolor ducimus excepturi facere in, inventore ipsa iste nemo neque nulla odio officia pariatur, perspiciatis quia quis quos rerum, sunt tenetur!Ad dolore earum ipsum unde.",
+        u1:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, ex!",
+        u2:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, ex!",
+        u3:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, ex!",
+        url:"https://www.google.com",
+        img:"https://placehold.it/66x66",
+        description:"There’s a lot of opportunity here to work on a wide range of very challenging projects and to grow quickly."
+    },
+    {   
+        id:"job2",
+        jobTtitle:"User Experience Designer",
+        type:"marketing",
+        rating:"4.2/5",
+        location:"Richmond, VA",
+        company:"Google inc.",
+        p1:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid animi delectus deleniti dicta dolor ducimus excepturi facere in, inventore ipsa iste nemo neque nulla odio officia pariatur, perspiciatis quia quis quos rerum, sunt tenetur Ad dolore earum ipsum unde.",
+        p2:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid animi delectus deleniti dicta dolor ducimus excepturi facere in, inventore ipsa iste nemo neque nulla odio officia pariatur, perspiciatis quia quis quos rerum, sunt tenetur!Ad dolore earum ipsum unde.",
+        p3:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid animi delectus deleniti dicta dolor ducimus excepturi facere in, inventore ipsa iste nemo neque nulla odio officia pariatur, perspiciatis quia quis quos rerum, sunt tenetur!Ad dolore earum ipsum unde.",
+        u1:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, ex!",
+        u2:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, ex!",
+        u3:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, ex!",
+        url:"https://www.google.com",
+        img:"https://placehold.it/66x66",
+        description:"There’s a lot of opportunity here to work on a wide range of very challenging projects and to grow quickly."
+    
+    },
+    {   
+        id:"job3",
+        jobTtitle:"User Experience Designer",
+        rating:"4.2/5",
+        location:"Richmond, VA",
+        company:"Google inc.",
+        p1:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid animi delectus deleniti dicta dolor ducimus excepturi facere in, inventore ipsa iste nemo neque nulla odio officia pariatur, perspiciatis quia quis quos rerum, sunt tenetur Ad dolore earum ipsum unde.",
+        p2:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid animi delectus deleniti dicta dolor ducimus excepturi facere in, inventore ipsa iste nemo neque nulla odio officia pariatur, perspiciatis quia quis quos rerum, sunt tenetur!Ad dolore earum ipsum unde.",
+        p3:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid animi delectus deleniti dicta dolor ducimus excepturi facere in, inventore ipsa iste nemo neque nulla odio officia pariatur, perspiciatis quia quis quos rerum, sunt tenetur!Ad dolore earum ipsum unde.",
+        u1:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, ex!",
+        u2:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, ex!",
+        u3:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, ex!",
+        url:"https://www.google.com",
+        img:"https://placehold.it/66x66",
+        description:"There’s a lot of opportunity here to work on a wide range of very challenging projects and to grow quickly."
+    
+    },
+]
+
+export default jobs;

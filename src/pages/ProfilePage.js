d7d8a938496419e463a9328eec9c3238e6ddbcd8
Sébastien Gorman <32175390+SebastienGorman@users.noreply.github.com>
2019-07-15 02:55:31 -0400

    My profile page, need more styling and logic

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Button, style} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 export default class ProfilePage extends React.Component {
@@ -9,8 +10,73 @@ export default class ProfilePage extends React.Component {
   render(){
     return (
       <div className="container">
-        <h1>Profile Page</h1>
-       
+        <main class="main">
+        <div class="container">
+          <div class="col-xs-12">
+
+            <div class="section-title-wrap">
+              <h3 class="section-title" style={{marginTop: "30px"}}>
+                My Profile
+              </h3>
+            </div>
+
+            <div class="row">
+              <div class="col-sm-6">
+
+                <p style={{marginTop: "30px"}}>Full Name</p>
+                <input style={{width: "100%"}} type="text" value="Michael Cloutier"/>
+
+              </div>
+
+              <div class="col-sm-6">
+
+                <p style={{marginTop: "30px"}}>Location</p>
+                <input style={{width: "100%"}} type="text" value="Ottawa, ON, Canada"/>
+
+              </div>
+
+              <div class="col-sm-12">
+
+                <p style={{marginTop: "30px"}}>Education</p>
+                <textarea rows = "5" cols = "145">
+                  TODO: Info about education
+                </textarea>
+
+                <p style={{marginTop: "30px"}}>Experience</p>
+                <textarea rows = "5" cols = "145">
+                  TODO: Info about education
+                </textarea>
+
+              </div>
+
+              <div class="col-sm-4">
+                <p style={{marginTop: "30px"}}>Resume</p>
+                <input type="file" id="myFile"/>
+              </div>
+
+              <div class="col-sm-8">
+                <Button
+                    className="pull-right"
+                    style={{marginTop: "40px", marginLeft: "20px"}}
+                    variant="outline-primary"
+                    type="submit"
+                    size="lg">
+                    Save Changes
+                  </Button>
+                  <Button
+                    className="pull-right"
+                    style={{marginTop: "40px", marginLeft: "20px"}}
+                    variant="default"
+                    type="submit"
+                    size="lg">
+                    Preview
+                  </Button>
+              </div>
+            </div>
+
+          </div>
+        </div>
+        </main>
       </div>
     )
   }

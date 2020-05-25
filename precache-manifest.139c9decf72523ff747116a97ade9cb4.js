a985762ad2c42589173e3e0ef02d98815f79dc75
Diedrick Ng <dngen049@uottawa.ca>
2020-05-25 13:07:22 -0400

    Updates

---
+++ precache-manifest.139c9decf72523ff747116a97ade9cb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fceed822fb0537434ab05c4fbb617b7e",
+    "url": "/coop-hack/index.html"
+  },
+  {
+    "revision": "d97537708957c8891cfc",
+    "url": "/coop-hack/static/css/2.1bea15ab.chunk.css"
+  },
+  {
+    "revision": "aaa6a6fd42394f6b99bb",
+    "url": "/coop-hack/static/css/main.1d5a74dc.chunk.css"
+  },
+  {
+    "revision": "d97537708957c8891cfc",
+    "url": "/coop-hack/static/js/2.b6ee1467.chunk.js"
+  },
+  {
+    "revision": "aaa6a6fd42394f6b99bb",
+    "url": "/coop-hack/static/js/main.4b46b4e5.chunk.js"
+  },
+  {
+    "revision": "c52a27d6147037766935",
+    "url": "/coop-hack/static/js/runtime~main.b5dcb6cc.js"
+  }
+]);
